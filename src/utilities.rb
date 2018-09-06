aef59ffd545ff14b6a30aaff5e1394ad292bf8b9
Mark Conanan <mark.conanan@gmail.com>
2018-09-06 13:42:02 +1000

    Presentation changes

--- src/utilities.rb
+++ src/utilities.rb
@@ -17,21 +17,20 @@ end
 
 # Arguments for error handling
 
-def validate_numarg (num)
-    raise NonNumericArgumentError, "Please specify a number" if !num.numeric?
+def validate_numarg (num) 
+    raise NonNumericArgumentError if !num.numeric?
     num
 end
 
 def validate_intarg (num)
-    raise NonIntegerArgumentError, "Please specify a number" if !num.is_i?
+    raise NonIntegerArgumentError if !num.is_i?
     num
 end
 
 # method to display the tables. this uses terminal-table gem, please see terminal-table gem for more documentation
 def report(rows)
-    current_time = DateTime.now
     table = Terminal::Table.new :headings => ['Exercise', 'Set', 'Weight', 'Reps'], 
-    :title => "Exercise Log #{current_time.strftime "%d/%m/%Y"}",
+    :title => "Exercise Log #{DateTime.now.strftime "%d/%m/%Y"}",
     :rows => rows
     puts `clear`    
     puts table
@@ -40,7 +39,6 @@ end
 # method to display the history. this uses terminal-table gem, please see terminal-table gem for more documentation
 # this is not yet implemented, there have to be changes on user flow to implement history
 def history_report(rows)
-    current_time = DateTime.now
     table = Terminal::Table.new :headings => ['Date','Exercise', 'Set', 'Weight', 'Reps'], 
     :title => "Exercise Log History",
     :rows => rows   
